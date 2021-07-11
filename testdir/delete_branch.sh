565bdcd195b84ccad2b47a6fa1f815aea7280695
Osy Ernesto Ekwonye <46828049+osygroup@users.noreply.github.com>
2021-07-11 12:39:14 -0500

    Update delete_branch.sh

--- testdir/delete_branch.sh
+++ testdir/delete_branch.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
-if [ DELBRANCH=$(git branch -r | grep -e testdel -e pier | sed 's/origin\///') ]; then
+DELBRANCH=$(git branch -r | grep -e testdel -e pier | sed 's/origin\///')
+BRANCH=$DELBRANCH 2>/dev/null
+if [ "$BRANCH" -gt "0" ]; then
     git push origin --delete $DELBRANCH
 fi
