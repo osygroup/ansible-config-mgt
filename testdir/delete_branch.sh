c7cfe8caf649c463195f88d43b179dd75a918df5
Osy Ernesto Ekwonye <46828049+osygroup@users.noreply.github.com>
2021-07-19 17:02:19 -0500

    Update delete_branch.sh

--- testdir/delete_branch.sh
+++ testdir/delete_branch.sh
@@ -8,14 +8,17 @@ fi
 
 
 for k in $(git branch -r | grep -v -e develop -e dev -e prod -e HEAD | sed /\*/d); do 
-  if [ -z "$(git log -1 --since='60 days ago' -s $k)" ]; then
+  if [ -z "$(git log -1 --since='200 days ago' -s $k)" ]; then
     echo "$k" >> prune.txt
-    echo Hello Service-Transcode team, 
-    echo Kindly prune the following old branches:
-    git for-each-ref --sort=-committerdate refs/remotes/ --format='%(refname:short) %(authorname) (%(committerdate:relative))' | grep -f prune.txt
   fi
 done
 
+RESULT=$(cat prune.txt 2>/dev/null)
 
+if [ "$RESULT" ]; then
+    echo Hello Service-Transcode team, 
+    echo 'Kindly prune the following old branches (last commit date in bracket):'
+    git for-each-ref --sort=-committerdate refs/remotes/ --format='%(refname:short) %(authorname) %(committerdate:relative)' | grep -f prune.txt
+fi
 
 # curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' ${{ secrets.SLACK_WEBHOOK }}
