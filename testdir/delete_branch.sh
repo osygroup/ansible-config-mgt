e00243e5e0853fba6aa33c1764f30576343a0308
Osy Ernesto Ekwonye <46828049+osygroup@users.noreply.github.com>
2021-07-19 11:00:27 -0500

    Update delete_branch.sh

--- testdir/delete_branch.sh
+++ testdir/delete_branch.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
-DELBRANCH=$(git branch -r | grep -e testdel -e pier | sed 's/origin\///')
+git fetch --prune
+DELBRANCH=$(git branch -r --merged origin/develop | grep -v -e develop -e dev -e prod -e HEAD | sed 's/origin\///')
 if [ "$DELBRANCH" ]; then
-    git push origin --delete $DELBRANCH
+    echo deleting branch $DELBRANCH
+    # git push origin --delete $DELBRANCH
 fi
+
+
+for k in $(git branch -r | grep -v -e develop -e dev -e prod -e HEAD | sed /\*/d); do 
+  if [ -z "$(git log -1 --since='60 days ago' -s $k)" ]; then
+    echo "$k" >> prune.txt
+  fi
+done
+
+echo Hello Service-Transcode team, 
+echo Kindly prune the following old branches:
+git for-each-ref --sort=-committerdate refs/remotes/ --format='%(refname:short) %(authorname) (%(committerdate:relative))' | grep -f prune.txt
