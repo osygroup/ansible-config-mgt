577420c2995609ee4e62fd4f387e9c3b448f1464
Osy Ernesto Ekwonye <46828049+osygroup@users.noreply.github.com>
2021-08-25 12:54:07 -0500

    Update cleanup-github-orphaned-branches.sh

--- scripts/cleanup-github-orphaned-branches.sh
+++ scripts/cleanup-github-orphaned-branches.sh
@@ -8,4 +8,4 @@ done
 #test
 git for-each-ref --sort=-committerdate refs/remotes/ --format='%(refname:short) %(authorname) (%(committerdate:relative))' | grep -w -f prune.txt >> list.txt
 
-curl -F file=@list.txt -F "initial_comment=Hello Service-Transcode team, kindly prune the following old branches (last commit date in bracket):" -F channels=C0294EZMK9N -H "Authorization: Bearer xoxb-2272093962023-2274771267591-7PnEiANCSZnH1HbGRQGRVZEu" https://slack.com/api/files.upload
+curl -F file=@list.txt -F "initial_comment=Hello Service-Transcode team, kindly prune the following old branches (last commit date in bracket):" -F channels=C0294EZMK9N -H "Authorization: Bearer xoxb-2272093962023-2274771267591-5tSamxW1Vl47tCxW8VJcEFa7" https://slack.com/api/files.upload
